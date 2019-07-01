8d9f0417ead5515d426d4a0ed8eb4899b3d3e93e
Ahmed <ahmed@pop-os.localdomain>
2019-07-01 20:55:08 +1200

    I renamed the title to MyBlog:)

--- storage/framework/views/9f99cb75c0417bf3867aba81af6cae4ec2f69920.php
+++ storage/framework/views/9f99cb75c0417bf3867aba81af6cae4ec2f69920.php
@@ -9,7 +9,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-        <title>Laravel</title>
+        <title>MyBlog</title>
 
         <!-- Fonts -->
         
